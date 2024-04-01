354ce9d597ac8d23db3279b845f26d0c33c95660
Arbalest <499624095@qq.com>
2024-04-01 15:59:20 +0800

    feat: update enum module mode

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -1,5 +1,14 @@
 // enum to select options
-export const enumToOptions = (v: Record<string, any>, isNumberEnum = true) => {
-    const options = Object.keys(v).map(key => ({ label: key, value: v[key] }))
-    return isNumberEnum ? options.filter(item => typeof item.value === 'number') : options
+export const enumToOptions = (v: Record<string, any>, isNumberEnum = true) =>
+    Object.keys(v)
+        .filter(key => !/[\u4e00-\u9fa5]/.test(v[key]))
+        .map(key => ({
+            label: key,
+            value: isNumberEnum ? Number(v[key]) : v[key]
+        }))
+
+import * as Common from './modules/common'
+
+export default {
+    Common
 }
---
+++ src/enum/modules/common.ts
@@ -0,0 +1,4 @@
+export enum Boolean {
+    '否',
+    '是'
+}
